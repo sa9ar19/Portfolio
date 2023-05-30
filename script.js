c25540d177c9f3e300b0905fbda50b95dbc02d04
sa9ar <khadka.xaagar01@gmail.com>
2023-05-30 17:24:49 +0545

    Home menu Only

---
+++ script.js
@@ -0,0 +1,44 @@
+var typed = new Typed('#element', {
+    strings: ["I am a Web Developer", "I am a Student", "I am a Photographer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+    });
+
+
+// var openMenu = document.getElementById("openMenu");
+
+// var closeMenu = document.getElementById("closeMenu");
+
+// var menuItems = document.getElementById("menuItems");
+
+// openMenu.addEventListener("click", show);
+
+// closeMenu.addEventListener("click",hide);
+
+// function show(){
+//     menuItems.style.display="block";
+//     menuItems.style.right="0px";
+//     closeMenu.style.display="block";
+// }
+
+// function hide(){
+//     menuItems.style.right="-1000px";
+//     closeMenu.style.display="none";
+// }
+
+// const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
